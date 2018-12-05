6ab5ff3785f9ef564f3fc3814ffc2308f5054f7e
Alistair Green <alistairmgreen@gmail.com>
2018-12-05 22:38:48 +0000

    Day 4, part 2.

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -5,13 +5,33 @@ fn main() -> Result<(), EventParseError> {
 
     let sleep_times = count_sleep_times(&events)?;
 
-    if let Some((id, sleep_count)) = sleep_times.iter().max_by_key(|(_, sleep)| sleep.iter().sum::<usize>()) {
+    if let Some((id, sleep_count)) = sleep_times
+        .iter()
+        .max_by_key(|(_, sleep)| sleep.iter().sum::<usize>())
+    {
         println!("Guard #{} spends the most time asleep.", id);
 
         if let Some((minute, _)) = sleep_count.iter().enumerate().max_by_key(|(_, c)| *c) {
-            println!("He is most often asleep at {} minutes past midnight.", minute);
+            println!(
+                "He is most often asleep at {} minutes past midnight.",
+                minute
+            );
 
             println!("The answer to Part 1 is therefore {}", id * minute);
+        }
+    }
+
+    if let Some((id, sleep_count)) = sleep_times
+        .iter()
+        .max_by_key(|(_, sleep)| sleep.iter().max().unwrap_or(&0))
+    {
+        if let Some((minute, times)) = sleep_count.iter().enumerate().max_by_key(|(_, c)| *c) {
+            println!(
+                "Guard #{} fell asleep {} times at {} minutes past midnight.",
+                id, times, minute
+            );
+
+            println!("The answer to Part 2 is therefore {}", id * minute);
         }
     }
 
@@ -21,7 +41,8 @@ fn main() -> Result<(), EventParseError> {
 fn read_events() -> Result<Vec<Event>, EventParseError> {
     let input = include_str!("puzzle_input.txt");
 
-    let mut events = input.lines()
+    let mut events = input
+        .lines()
         .map(|line| line.parse())
         .collect::<Result<Vec<Event>, EventParseError>>()?;
 
